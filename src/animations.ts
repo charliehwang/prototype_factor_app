7bc760ea9b19003758250a4ad286ea68c51fd7f6
charlie <charliehwang@gmail.com>
2021-06-09 19:36:21 -0600

    Fixed bugs in scrollbar for charts and errors in the console

--- src/animations.ts
+++ src/animations.ts
@@ -12,23 +12,29 @@ import {
 export const slideInAnimation = trigger('routeAnimations', [
   transition('* <=> *', [
     style({ position: 'relative' }),
-    query(':enter, :leave', [
-      style({
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
-      }),
-    ]),
-    query(':enter', [style({ opacity: '0' })]),
+    query(
+      ':enter, :leave',
+      [
+        style({
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+        }),
+      ],
+      { optional: true }
+    ),
+    query(':enter', [style({ opacity: '0' })], { optional: true }),
     query(':leave', animateChild(), { optional: true }),
     group([
       query(':leave', [animate('200ms ease-out', style({ opacity: '0' }))], {
         optional: true,
       }),
-      query(':enter', [animate('200ms ease-out', style({ opacity: '1' }))]),
+      query(':enter', [animate('200ms ease-out', style({ opacity: '1' }))], {
+        optional: true,
+      }),
     ]),
-    query(':enter', animateChild()),
+    query(':enter', animateChild(), { optional: true }),
   ]),
 ]);
